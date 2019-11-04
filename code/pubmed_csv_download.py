c9d6c601f62e0b674b067f9baec16ba5e5690e1d
Xuan Gu <guxuan@msn.com>
2019-11-04 14:03:16 +0100

    Update pubmed_csv_download.py

--- code/pubmed_csv_download.py
+++ code/pubmed_csv_download.py
@@ -1,60 +1,33 @@
-import logging
 import requests
 import csv
 import os
+import xml.etree.ElementTree as ET
 
-TOOLNAME = 'pubmed_pdf'
-EMAIL = 'your_email@example.com'  # Please set if you use this extensively
+def save_xml(pmcid, save_files):
+    oa_url = 'https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?id=' + pmcid
+    response = requests.get(oa_url)
+    root = ET.fromstring(response.text)
+    tree = ET.ElementTree(root)
+    save_dir = save_files + 'xml/'
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    tree.write(save_dir + pmcid + '.xml')
 
-API_URL = "https://www.ncbi.nlm.nih.gov/pmc/utils/idconv/v1.0/?tool={toolname}&email={email}&ids={pmid}&versions=no&format=json"
-PMC_PDF_URL = "https://www.ncbi.nlm.nih.gov/pmc/articles/{pmcid}/pdf/"
-PMC_ePUB_URL = "https://www.ncbi.nlm.nih.gov/pmc/articles/{pmcid}/epub/"
+def get_download_url(pmcid):
+    oa_url = 'https://www.ncbi.nlm.nih.gov/pmc/utils/oa/oa.fcgi?id=' + pmcid
+    response = requests.get(oa_url)
+    oa_xml = response.text
+    tar_url = oa_xml[oa_xml.find('ftp'):oa_xml.find('.tar.gz')+7]
+    pdf_url = oa_xml[oa_xml.rfind('ftp'):oa_xml.find('.pdf')+4]
+    return tar_url, pdf_url
 
-logging.basicConfig()
-logger = logging.getLogger('pubmed_pdf')
-
-def find_url(url):
-    r = requests.head(url, headers={'User-Agent': 'fetch_pmc_pdf/0.1'})
-    if r.status_code == 200:
-        return url
-    elif r.status_code == 303:  # redirect
-        return find_url(r.headers['Location'])
-    else:
-        return None
-
-def find_url2(url):
-    r = requests.head(url, headers={'User-Agent': 'fetch_pmc_epub/0.1'})
-    if r.status_code == 200:
-        return url
-    elif r.status_code == 303:  # redirect
-        return find_url(r.headers['Location'])
-    else:
-        return None
-
-
-def find_pmc_url(pmcid):
-    # pmcid = pubmed_to_pmc(pmid)
-    # if pmcid is not None:
-    logger.debug('PMCID: %s', pmcid)
-    # Check whether PMC has a pdf
-    url = PMC_PDF_URL.format(pmcid=pmcid[3:], toolname=TOOLNAME, email=EMAIL)
-    url = find_url(url)
-    file_format = 'pdf'
-    if url is None:
-        url = PMC_ePUB_URL.format(pmcid=pmcid[3:], toolname=TOOLNAME, email=EMAIL)
-        url = find_url2(url)
-        file_format = 'epub'
-    return url, file_format
-    # return None
-
-# def pubmed_to_pmc(pmid):
-#     url = API_URL.format(pmid=pmid, toolname=TOOLNAME, email=EMAIL)
-#     reply = requests.get(url).json()
-#     records = reply['records'][0]
-#     pmcid = records.get('pmcid', None)
-#     return pmcid
-
-def download_pdf(pubmed_csv_dir, save_articles, skiprows, nrows):
+def download_articles(pubmed_csv_dir, save_files, skiprows, nrows):
+    save_tar_dir = save_files + 'tar/'
+    if not os.path.exists(save_tar_dir):
+        os.makedirs(save_tar_dir)
+    save_pdf_dir = save_files + 'pdf/'
+    if not os.path.exists(save_pdf_dir):
+        os.makedirs(save_pdf_dir)
     # with open(pubmed_csv_dir, "rt") as f_obj_int, open(pubmed_csv_filtered_dir, 'wt') as f_obj_out:
     with open(pubmed_csv_dir, "rt") as f_obj_int:
         # writer = csv.writer(f_obj_out, dialect='excel')
@@ -73,25 +46,28 @@ def download_pdf(pubmed_csv_dir, save_articles, skiprows, nrows):
             print(article_title)
             if 'PMCID' in pmid_pmcid:
                 pmcid = pmid_pmcid[-10:]
-                article_url, file_format = find_pmc_url(pmcid)
-                if article_url is not None:
-                    print(article_url)
-                    if 'pdf' in file_format:
-                        os.system('wget --user-agent="Mozilla" ' + article_url + ' -O ' + save_articles + pmcid + '.pdf')
-                    else:
-                        os.system('wget --user-agent="Mozilla" ' + article_url + ' -O ' + save_articles + pmcid + '.epub')
+                save_xml(pmcid, save_files)
+                tar_url, pdf_url = get_download_url(pmcid)
+                if tar_url != '':
+                    print(tar_url)
+                    os.system('wget --user-agent="Mozilla" ' + tar_url + ' -O ' + save_tar_dir + pmcid + '.tar.gz --quiet &')
                 else:
-                    print('No pdf or epub found')
+                    print('No tar found!')
+                if pdf_url != '':
+                    print(pdf_url)
+                    os.system('wget --user-agent="Mozilla" ' + pdf_url + ' -O ' + save_pdf_dir + pmcid + '.pdf --quiet &')
+                else:
+                    print('No pdf found!')
             print('\n')
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument('-pubmed_csv', help='The csv file created from PubMed')
-    parser.add_argument('-save_articles', help='The directry to save the pdf files')
+    parser.add_argument('-save_files', help='The directry to save the articles')
     parser.add_argument('-skiprows',type=int, default=1, help='Skip the some rows in the csv')
     parser.add_argument('-nrows',type=int, default=None, help='Read the rows after skipping')
 
     # parser.add_argument('-save_csv', help='The directry to save the filtered csv')
     args = parser.parse_args()
-    download_pdf(args.pubmed_csv, args.save_articles, args.skiprows, args.nrows)
+    download_articles(args.pubmed_csv, args.save_files, args.skiprows, args.nrows)
